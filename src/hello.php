a675c38b5096007447ccac87dfccc4cc6c7de67c
ravalabhijit85 <64782026+ravalabhijit85@users.noreply.github.com>
2020-07-12 19:18:56 +0530

    Create hello.php

---
+++ src/hello.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) Abhijit Raval
+ *
+ * This source file
+ */
+
+
+// namespace HelloComposer;
+namespace MyComposer\HelloComposer;
+
+class Hello {
+
+	public function say( $toSay = "Nothing given" ) {
+		return $toSay;
+	}
+}
+?>
